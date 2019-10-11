e616333a1da84e106fa3de567403c3ae02c0e240
Zhongqiang Shen <shenzhongqiang@msn.com>
2019-10-10 20:55:37 -0700

    enlarge output image

--- spiral_portrait.py
+++ spiral_portrait.py
@@ -1,7 +1,8 @@
 import numpy as np
 import cv2
 
-RATIO = 1.5
+B = 1.5
+RATIO = 3
 
 def get_spiral_points(center, i):
     base_theta = np.pi/180 * i
@@ -9,37 +10,78 @@ def get_spiral_points(center, i):
     result = []
     for j in range(num):
         theta = base_theta + j*np.pi/180/num
-        r = theta * RATIO
+        r = theta * B
         x = int(center[0] + r*np.cos(theta))
         y = int(center[1] + r*np.sin(theta))
         result.append((x, y))
     return result
 
-def draw_spiral(img, center, max_r):
+def draw_with_color():
+    im = cv2.imread("yuanyuan.jpg")
+    imgray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    shape = imgray.shape
+    mask = np.ones(shape) * 255
+    width = shape[1]
+    height = shape[0]
+    center = (int(width/2), int(height/2))
+    max_r = np.min(center)
+
     points = []
-    num = int(max_r/np.pi*180/RATIO)
+    num = int(max_r/np.pi*180/B)
     sizes = np.random.normal(2, 1, size=num).astype('uint8')
     sizes[sizes>3]=3
     sizes[sizes<2]=2
     sizes = np.random.randint(2, 4, size=num)
-    print(np.min(sizes))
+
     for i in range(num):
         points = get_spiral_points(center, i)
         for point in points:
-            cv2.circle(img, point, 1, 0, sizes[i])
+            cv2.circle(mask, point, radius=sizes[i], color=0, thickness=-1)
 
-im = cv2.imread("yuanyuan.jpg")
-imgray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-shape = imgray.shape
-mask = np.ones(shape) * 255
-width = shape[1]
-height = shape[0]
-center = (int(width/2), int(height/2))
-max_r = np.max(center)
-draw_spiral(mask, center, max_r)
-mask = mask.astype(imgray.dtype)
-res = cv2.bitwise_or(imgray,mask)
-cv2.imshow("output", res)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    mask = mask.astype(imgray.dtype)
+    res = cv2.bitwise_or(imgray,mask)
+    cv2.imshow("output", res)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
+def draw_with_thickness():
+    im = cv2.imread("yuanyuan.jpg")
+    imgray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    shape = imgray.shape
+    newshape = np.multiply(shape, RATIO)
+    res = np.ones(newshape) * 255
+    width = newshape[1]
+    height = newshape[0]
+    center = (int(width/2), int(height/2))
+    max_r = np.min(center)
+
+    spiral_points = []
+    num = int(max_r/np.pi*180/B)
+    sizes = np.random.normal(2, 1, size=num).astype('uint8')
+    sizes[sizes>3]=3
+    sizes[sizes<2]=2
+    sizes = np.random.randint(2, 4, size=num)
+
+    for i in range(num):
+        points = get_spiral_points(center, i)
+        spiral_points.extend(points)
+
+    min_color = np.min(imgray)
+    max_color = np.max(imgray)
+    samples = np.linspace(min_color, max_color, 4)
+
+    for point in spiral_points:
+        cord = (int(point[1]/RATIO), int(point[0]/RATIO))
+        color = imgray[cord]
+        size = 4-np.argmax(samples>=color)
+        cv2.circle(res, point, radius=size, color=0, thickness=-1)
+
+    cv2.imwrite("output.jpg", res)
+    cv2.imshow("output", res)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+
+if __name__ == "__main__":
+    draw_with_thickness()
